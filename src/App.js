091ceb02077d12b6954dff51033db61f8105d525
Gepuy <maxbondarev93@gmail.com>
2022-05-26 23:36:53 +0300

    imported firebase, completed Main page,added env file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import {AuthContextProvider} from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 
 function App() {
   return (
     <>
-        <Navbar />
-        <Routes>
-            <Route path={'/'} element={<Home/>}/>
-        </Routes>
+        <AuthContextProvider>
+            <Navbar />
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/signup'} element={<Signup/>}/>
+                <Route path={'/account'} element={<Account/>}/>
+            </Routes>
+        </AuthContextProvider>
+
     </>
   );
 }
